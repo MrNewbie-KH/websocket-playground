6f23bda95aba5b93e848a1d0b57eef65279c345d
khattab <mkhattab369@gmail.com>
2025-09-17 01:05:44 +0300

    adding websocket configuration
    stomp endpoint to create connection - messagebroer configuration to complete the flow of stomp frames to be handled

---
+++ src/main/java/playground/websocketchat/configuration/Config.java
@@ -0,0 +1,23 @@
+package playground.websocketchat.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class Config implements WebSocketMessageBrokerConfigurer {
+//    two parts
+//    part 1 handshake and connect
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/websocket").withSockJS();
+    }
+//    part 2 message handling and broker
+    public void configureMessageBroker(MessageBrokerRegistry registry){
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+
+    }
+}
